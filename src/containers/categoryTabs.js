55c5020c12095416c1666bbb4e05cfbec39b96f9
Raghav <raghav.agrawal@83incs.com>
2021-09-11 00:22:35 +0530

    code with question's answer at category tabs page

--- src/containers/categoryTabs.js
+++ src/containers/categoryTabs.js
@@ -24,6 +24,29 @@ class CategoryTabs extends React.Component {
 		}
 	}
 
+	count(string, position) {
+		let counter = 0;
+		let copyString = string;
+		string = string.split('');
+
+		if (string.length < position) {
+			const diff = position - string.length;
+			for (let i = 0; i <= diff; i++) {
+				string.push(copyString[i % 3]);
+			}
+		}
+
+		console.log('string: ', string);
+
+		for (let i = 0; i < position; i++) {
+			if (string[i] === 'a') {
+				counter++;
+			}
+		}
+
+		return counter;
+	}
+
 	categoryClickHandler = (name, id) => {
 		this.setState({
 			categoryId: id,
@@ -80,11 +103,18 @@ class CategoryTabs extends React.Component {
 	}
 
 	render() {
+
+		// const string = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It a';
+		// const position = 10000000;
+		// let counterForA = this.count(string, position);
+		// console.log('counterForA: ', counterForA);
+
 		return <div>
 			<div className="category-outer-box">
 				{this.state.categoryTabs.map(val => <div
 					onClick={() => this.categoryClickHandler(val.category_name, val.category_id)}
 					key={val.category_id}
+					id={val.category_name}
 					className="cat-tab">
 					<img src={val.category_image} alt="category Img" />
 					<span>{val.category_name}</span>
@@ -110,12 +140,12 @@ class CategoryTabs extends React.Component {
 					open={Boolean(this.state.anchorEl)}
 					onClose={this.handleClose}
 				>
-					{this.state.categoryTabs.map(val => <MenuItem
+					{this.state.categoryTabs.map(val => <a href={'#' + val.category_name} > <MenuItem
 						key={val.category_id}
-						onClick={() => this.handleClose(val.category_name, val.category_id)}>{val.category_name}</MenuItem>)}
+						onClick={() => this.handleClose(val.category_name, val.category_id)}>{val.category_name}</MenuItem></a>)}
 				</Menu>
 			</div>
-		</div>
+		</div >
 	}
 }
 
